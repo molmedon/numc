b46a5189030d610571a56c097183b4940ced3584
Remy Prechelt <rprechelt@gmail.com>
2018-03-10 03:42:04 +0000

    Moved neutrino tests into neutrino test file

--- test/test_Particle.cpp
+++ test/test_Particle.cpp
@@ -3,32 +3,7 @@
 
 TEST_SUITE_BEGIN("particle");
 
-TEST_CASE("Creating a base neutrino") {
 
-    SUBCASE("Electron Neutrino") {
-        anita::Neutrino neutrino = anita::Neutrino(18., anita::Flavor::Electron);
-    }
-
-    SUBCASE("Muon Neutrino") {
-        anita::Neutrino neutrino = anita::Neutrino(18., anita::Flavor::Muon);
-    }
-
-    SUBCASE("Tau Neutrino") {
-        anita::Neutrino neutrino = anita::Neutrino(18., anita::Flavor::Tau);
-    }
-}
-
-TEST_CASE("Creating an electron neutrino") {
-    anita::ElectronNeutrino neutrino = anita::ElectronNeutrino(18.);
-}
-
-TEST_CASE("Creating an muon neutrino") {
-    anita::MuonNeutrino neutrino = anita::MuonNeutrino(18.);
-}
-
-TEST_CASE("Creating an tau neutrino") {
-    anita::TauNeutrino neutrino = anita::TauNeutrino(18.);
-}
 
 
 TEST_SUITE_END();
